3cc08709a944d67c398a848b97ec96ea1c52b164
abidinz <zainal.abi@gmail.com>
2023-12-22 10:03:16 +0700

    fix schema folder

--- cap-check.py
+++ cap-check.py
@@ -4,6 +4,6 @@ import xmlschema
 
 file_name = sys.argv[1]
 cap_version = sys.argv[2]
-schema = xmlschema.XMLSchema(f"./cap-library/schema/{cap_version}.xsd")
+schema = xmlschema.XMLSchema(f"./google-cap/schema/{cap_version}.xsd")
 
 print(schema.is_valid(file_name))
